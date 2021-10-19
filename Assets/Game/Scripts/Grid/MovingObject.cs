3fb4d7ca3487f3eae7ee5b9c19928a75be19ce6f
Takashi <bt.tengan@gmail.com>
2021-10-19 00:56:39 -0300

    keep moving behaviour

--- Assets/Game/Scripts/Grid/MovingObject.cs
+++ Assets/Game/Scripts/Grid/MovingObject.cs
@@ -8,57 +8,73 @@ public class MovingObject : MonoBehaviour
     public bool IsMoving => _moveCoroutine != null;
     
     [SerializeField] protected float _speed = 1;
-    Vector2Int _moveDirection = Vector2Int.up;
-    Vector2Int _desiredDirection = Vector2Int.up;
+    Vector3Int _moveDirection = Vector3Int.up;
+    Vector3Int _desiredDirection = Vector3Int.up;
     Coroutine _moveCoroutine = null;
-
-    void Awake() {
-    }
     
     void Start() {
         var cellPosition = GridBoard.Instance.GetPositionWorldToCell(transform.position);
         transform.position = GridBoard.Instance.GetPositionCellToWorld(cellPosition);
+        // OnMovedToCell += KeepMoving;
     }
 
-    void Update() {
-        if (Input.GetKeyDown(KeyCode.UpArrow)) {
-            ChangeDirection(Vector2Int.up);
-        }
-        if (Input.GetKeyDown(KeyCode.DownArrow)) {
-            ChangeDirection(Vector2Int.down);
-        }
-        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
-            ChangeDirection(Vector2Int.left);
-        }
-        if (Input.GetKeyDown(KeyCode.RightArrow)) {
-            ChangeDirection(Vector2Int.right);
-        }
-    }
+    // void Update() {
+    //     if (Input.GetKeyDown(KeyCode.UpArrow)) {
+    //         ChangeDirection(Vector2Int.up);
+    //     }
+    //     if (Input.GetKeyDown(KeyCode.DownArrow)) {
+    //         ChangeDirection(Vector2Int.down);
+    //     }
+    //     if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+    //         ChangeDirection(Vector2Int.left);
+    //     }
+    //     if (Input.GetKeyDown(KeyCode.RightArrow)) {
+    //         ChangeDirection(Vector2Int.right);
+    //     }
+    // }
 
+    #region Protected Methods
     protected void ChangeDirection(Vector2Int p_direction) {
-        _desiredDirection = p_direction;
+        _desiredDirection = (Vector3Int)p_direction;
         if (CanChangeDirection()) {
             _moveDirection = _desiredDirection;
-            StartMoveToCell(GetCellOnDirection((Vector3Int)_moveDirection));
+            StartMoveToCell(GetCellOnDirection(_moveDirection));
         }
     }
 
+    protected void KeepMoving() {
+        if (CanChangeDirection()) {
+            _moveDirection = _desiredDirection;
+            StartMoveToCell(GetCellOnDirection(_moveDirection));
+        }
+        else if (IsWalkableOnDirection(_moveDirection))  {
+            StartMoveToCell(GetCellOnDirection(_moveDirection));
+        }
+    }
+    #endregion
+
+    #region Bool checks
     bool CanChangeDirection() {
         if (!IsOnCellPosition()) {
             if (!IsSameWayToMovement(_desiredDirection)) {
                 return false;
             }
         }
-        return GridBoard.Instance.IsTileWalkable(GetCellOnDirection((Vector3Int)_desiredDirection));
+        return IsWalkableOnDirection(_desiredDirection);
     }
 
     bool IsOnCellPosition() {
         return (Vector2)transform.position == (Vector2)GridBoard.Instance.GetPositionWorldToWorld(transform.position);
     }
 
-    bool IsSameWayToMovement(Vector2Int p_direction) {
+    bool IsSameWayToMovement(Vector3Int p_direction) {
         return p_direction == _moveDirection || p_direction == -_moveDirection;
     }
+
+    bool IsWalkableOnDirection(Vector3Int p_direction) {
+        return GridBoard.Instance.IsTileWalkable(GetCellOnDirection(p_direction));
+    }
+    #endregion
 
     Vector3Int GetCellOnDirection(Vector3Int p_direction) {
         Vector3Int currentCell = GridBoard.Instance.GetPositionWorldToCell(transform.position);
@@ -67,15 +83,13 @@ public class MovingObject : MonoBehaviour
         if (IsOnCellPosition()) {
             return directionCell;
         }
-        if (!IsSameWayToMovement((Vector2Int)p_direction)) {
+        if (!IsSameWayToMovement(p_direction)) {
             return directionCell;
         }
         
-        // Vector3 currentPosition = GridBoard.Instance.GetPositionCellToWorld(currentCell);
         Vector3 directionPosition = GridBoard.Instance.GetPositionCellToWorld(directionCell);
-        // Vector3 diffCurrent = transform.position - currentPosition;
         Vector3 diffDirection = transform.position - directionPosition;
-        if (diffDirection.magnitude > 1) { // Considerando que todos os tiles possuem tamanho de 1 x 1.
+        if (diffDirection.magnitude > 1) { // Considerando que todos os tiles possuem tamanho de 1x1.
             return currentCell;
         }
         else {
@@ -83,6 +97,7 @@ public class MovingObject : MonoBehaviour
         }
     }
 
+    #region Movement
     void StartMoveToCell(Vector3Int p_cellPosition) {
         if (_moveCoroutine != null) {
             StopCoroutine(_moveCoroutine);
@@ -101,4 +116,5 @@ public class MovingObject : MonoBehaviour
         _moveCoroutine = null;
         OnMovedToCell?.Invoke();
     }
+    #endregion
 }
